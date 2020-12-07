ae2dea6b49f757922e83db1fe027000c1bb6b05b
Daniel Williams <dw569@exeter.ac.uk>
2020-12-07 15:16:46 +0000

    Further correction to previous commit

--- src/atmos_param/two_stream_gray_rad/two_stream_gray_rad.F90
+++ src/atmos_param/two_stream_gray_rad/two_stream_gray_rad.F90
@@ -450,8 +450,6 @@ if (do_seasonal) then
       r_solday=real(solday)
       frac_of_year = (r_solday*day_in_s) / year_in_s
   else
-      r_days=real(days)
-      r_total_seconds=r_seconds+(r_days*day_in_s)
       frac_of_year = r_total_seconds / year_in_s
   endif
 
