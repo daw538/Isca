73435445be42f5b0690beaed59d927ea146a453f
Daniel Williams <dw569@exeter.ac.uk>
2021-11-19 16:42:30 +0000

    Attempts to introduce enforced RH profile

--- src/atmos_column/column.F90
+++ src/atmos_column/column.F90
@@ -25,6 +25,7 @@ use           spec_mpp_mod, only: spec_mpp_init, get_grid_domain, grid_domain
 use       time_manager_mod, only: time_type, get_time, set_time, get_calendar_type, NO_CALENDAR, &
                                   get_date, interval_alarm, operator( - ), operator( + )
 use        tracer_type_mod, only: tracer_type, tracer_type_version, tracer_type_tagname
+use     sat_vapor_pres_mod, only: lookup_es
 
 
 implicit none
@@ -37,7 +38,7 @@ character(len=128), parameter :: tagname = '$Name: isca_201811 $'
 
 integer :: id_ps, id_u, id_v, id_t
 integer :: id_pres_full, id_pres_half, id_zfull, id_zhalf
-integer :: id_sphum_vert_int
+integer :: id_sphum_vert_int, id_enforced_rh
 integer, allocatable, dimension(:) :: id_tr
 character(len=8) :: mod_name = 'column'
 integer, dimension(4) :: axis_id
@@ -97,6 +98,10 @@ logical :: json_logging = .false.
 
 real, dimension(2) :: valid_range_t = (/100.,500./)
 
+logical :: enforce_col_rh = .false.
+character(len=256) :: rh_prof_file_name  = 'INPUT/rh_profile.nc'
+character(len=256) :: rh_prof_field_name = 'rh'
+
 namelist /column_nml/ use_virtual_temperature, valid_range_t, &
                       lon_max, lat_max, num_levels, &
                       print_interval, vert_coord_option, &
@@ -104,7 +109,11 @@ namelist /column_nml/ use_virtual_temperature, valid_range_t, &
                       reference_sea_level_press, scale_heights, surf_res, &
                       p_press, p_sigma, exponent, &
                       initial_state_option, initial_sphum, graceful_shutdown, &
-                      raw_filter_coeff, robert_coeff, json_logging
+                      raw_filter_coeff, robert_coeff, json_logging, &
+                      enforce_col_rh, rh_prof_file_name, rh_prof_field_name
+                      
+real, allocatable, dimension(:,:,:) :: rh_prof_inputs
+real, allocatable, dimension(:,:,:,:) :: enforced_rh ! (lat,lon,pfull,time)
 
 
 contains 
@@ -209,6 +218,32 @@ subroutine column_init(Time, Time_step_in, tracer_attributes, dry_model_out, nhu
     call set_domain(grid_domain)
     call get_time(Time_step, seconds, days)
     dt_real = 86400*days + seconds
+    
+    if (enforce_col_rh) then
+      if(file_exist(trim(rh_prof_file_name))) then
+        call error_mesg('column','Using RH profile specified by input file.', NOTE)
+        !call mpp_get_global_domain(grid_domain, xsize=global_num_lon, ysize=global_num_lat)
+        !call field_size(trim(rh_prof_file_name), trim(rh_prof_field_name), siz)
+
+      !if ( siz(1) == global_num_lon .or. siz(2) == global_num_lat ) then
+        call read_data(trim(rh_prof_file_name), trim(rh_prof_file_name), rh_prof_inputs, grid_domain)
+      !    where(land)
+      !    enforced_rh(:,:,:,1)  = rh_prof_inputs
+
+      !else
+      !  write(ctmp1(1: 4),'(i4)') siz(1)
+      !  write(ctmp1(9:12),'(i4)') siz(2)
+      !  write(ctmp2(1: 4),'(i4)') global_num_lon
+      !  write(ctmp2(9:12),'(i4)') global_num_lat
+      !  call error_mesg ('column','RH input profile file contains data on a '// &
+      !         ctmp1//' grid, but atmos model grid is '//ctmp2, FATAL)
+      
+
+      else
+        call error_mesg('column','Enforced RH input profile called' // &
+                     ' but '//trim(rh_prof_file_name)//' does not exist', FATAL)
+      endif
+    endif
 
     module_is_initialized = .true.
     ! NTL: CHECK AGAINST spectral_dynamics_init TO SEE WHAT ELSE NEEDS TO BE INITIALISED 
@@ -243,6 +278,9 @@ integer :: ntr
 
 logical :: pe_is_valid = .true.
 logical :: r_pe_is_valid = .true.
+
+real, dimension(is:is, js:js, num_levels)  :: enforced_e
+real, dimension(is:is, js:js, num_levels)  :: enforced_es
 
 ! THIS IS WHERE I NEED TO START FROM... 
 ! TO DO: LOCAL VARIABLES, CHECK INPUTS, HOOK UP TO ATMOSPHERE.F90 AND GLOBAL VARIABLE DEFINTIONS 
@@ -345,6 +383,13 @@ do ntr = 1, num_tracers
   call leapfrog_3d_real(grid_tracers(:,:,:,:,ntr),dt_tracers_tmp(:,:,:,ntr),previous,current,future,delta_t,tracer_attributes(ntr)%robert_coeff, raw_filter_coeff)
 enddo 
 
+if (enforce_col_rh) then
+  call LOOKUP_ES(tg(:,:,:,current), enforced_es)
+  enforced_e = enforced_es * rh_prof_inputs
+  grid_tracers(:,:,:,current,nhum) = (enforced_e * (rdgas/rvgas))/(p_full + ((rdgas/rvgas) - 1)*enforced_e)   
+  !grid_tracers(:,:,:,current,nhum) = enforced_q
+endif
+
 
 previous = current
 current  = future
@@ -352,6 +397,8 @@ current  = future
 call get_time(Time, seconds, days)
 seconds = seconds + step_number*int(dt_real/2)
 Time_diag = set_time(seconds, days)
+
+
 
 enddo step_loop
 
@@ -411,10 +458,12 @@ subroutine column_diagnostics_init(Time)
   id_pk = register_static_field(mod_name, 'pk', (/id_phalf/), 'vertical coordinate pressure values', 'pascals')
   id_bk = register_static_field(mod_name, 'bk', (/id_phalf/), 'vertical coordinate sigma values', 'none')
   id_zsurf = register_static_field(mod_name, 'zsurf', (/id_lon,id_lat/), 'geopotential height at the surface', 'm')
+  id_enforced_rh = register_static_field(mod_name, 'enforced_rh', (/id_phalf/), 'enforced relative humidity profile', 'percent')
   
   if(id_pk    > 0) used = send_data(id_pk, pk, Time)
   if(id_bk    > 0) used = send_data(id_bk, bk, Time)
   if(id_zsurf > 0) used = send_data(id_zsurf, surf_geopotential/grav, Time)
+  if(id_enforced_rh > 0) used = send_data(id_enforced_rh, rh_prof_inputs(1,1,:), Time)
 
   id_ps  = register_diag_field(mod_name, &
         'ps', (/id_lon,id_lat/),       Time, 'surface pressure',             'pascals')
@@ -442,6 +491,7 @@ subroutine column_diagnostics_init(Time)
         
   id_sphum_vert_int   = register_diag_field(mod_name, &
         'sphum_vert_int', (/id_lon,id_lat/),    Time, 'vertical column mass of specific humidity', 'kg/m2')
+
 
   allocate(id_tr(num_tracers))
   do ntr=1,num_tracers
@@ -540,6 +590,7 @@ subroutine column_diagnostics(Time, p_surf, u_grid, v_grid, t_grid, wg_full, tr_
     deallocate(pk, bk)
     deallocate(surf_geopotential)
     deallocate(grid_tracers)
+    deallocate(enforced_rh)
     
     call column_diagnostics_end
     call press_and_geopot_end
@@ -618,6 +669,9 @@ subroutine allocate_fields
     allocate (surf_geopotential(is:ie, js:je))
     
     allocate (grid_tracers(is:ie, js:je, num_levels, num_time_levels, num_tracers))
+    
+    allocate (rh_prof_inputs (is:ie, js:je, num_levels)) ; rh_prof_inputs=0.
+    allocate (enforced_rh (is:ie, js:je, num_levels, num_time_levels)) ; enforced_rh=0.
     
     ! Filling allocatable arrays with zeros immediately after allocation facilitates code debugging
     psg=0.; ug=0.; vg=0.; tg=0.
